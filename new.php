5c72ce20f0befe285464fcdf0383ab29c89b4fc7
sadique <mohamedsadique969@gmail.com>
2017-02-24 18:38:08 +0530

    fn not responsive

---
+++ new.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Facebbok</title>
+	</head>
+	<body>
+		<table width="100%" cellspacing="0" >
+			<tr bgcolor="#3b5998">
+				<td width="55%"><img src="images/logo.png" width="320"></td>
+				<td align="left"><input type="text" name="username" placeholder="Email or Phone">   <input type="password" name="pass" placeholder="Password">  <input type="button" value="LOGIN"><br><br><font color="white" style="padding-left: 0px;">Forgot Password ?</font>
+				</td>
+			</tr>
+			<tr bgcolor="#edf0f5" height="600px">
+				<td valign="top" align="center"><br><br>
+					<table width="80%" >
+						<tr>
+							<td>
+								<h2><font face="verdana">Connect with friends and the world around you on facebook.</font></h2>
+							</td>
+						</tr>
+						<tr>
+							<td><img src="images/connect.png"></td>
+						</tr>
+						<tr>
+							<td>
+								<font face="verdana"><br>
+									<img src="images/fblogo.png">See photos and updates from friends in news feed.<br><br>
+									<img src="images/fblogo.png">Share what`s new in your life on your time line.<br><br>
+									<img src="images/fblogo.png">Find more of what you are looking for with graph search.<br><br>
+								</font>
+							</td>
+						</tr>
+					</table>
+					<td valign="top" align="center" >
+						<h1>Create an account</h1>
+						<table cellspacing="10" >
+							<tr>
+								<td>
+									<input type="text" name="fname" placeholder="First name" style="height: 20px;size: 20px;width: 200px">
+								</td>
+								<td>
+									<input type="text" name="sname" placeholder="Second name" style="height: 20px;size: 20px;width: 180px;">
+								</td>
+							</tr>
+							<tr>
+								<td colspan="2">
+									<input type="text" name="mob" placeholder="Mobile Number" style="height: 20px;width:400px;">
+								</td>
+							</tr>
+							<tr>
+								<td colspan="2">
+									<input type="Email" name="email" placeholder="E-mail" style="height: 20px;width:400px;">
+								</td>
+							</tr>
+							<tr>
+								<td colspan="2">
+									<input type="password" name="pass" placeholder="New Password" style="height: 20px;width:400px;">
+								</td>
+							</tr>
+							<tr>
+								<td colspan="2">
+									<input type="password" name="pass" placeholder="Confirm Password" style="height: 20px;width:400px;">
+								</td>
+							</tr>
+							<tr>
+								<td colspan="2">
+									<input type="radio" name="">Male <input type="radio" name="">Female
+								</td>
+							</tr>
+							<tr>
+								<td colspan="2" width="350px"><a href=""><img src="images/signup.png"></a></td>
+							</tr>
+						</table>
+				</td>
+			</td>
+		</table>
+	</body>
+</html>
